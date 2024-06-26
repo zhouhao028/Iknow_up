fced16030f6e210647e724a04343bcdf58dfe505
zhouhao028 <152792064+zhouhao028@users.noreply.github.com>
2024-06-26 16:33:06 +0800

    Update run_llava.py

--- run_llava.py
+++ run_llava.py
@@ -3,8 +3,7 @@ import torch
 import requests
 from PIL import Image
 from io import BytesIO
-import json
-import os
+import re
 from transformers import TextStreamer
 
 from llava.constants import (
@@ -24,19 +23,13 @@ from llava.mm_utils import (
     KeywordsStoppingCriteria,
 )
 
-def parse_jsonl(file_path):
-    data = []
-    with open(file_path, 'r') as f:
-        for line in f:
-            data.append(json.loads(line))
-    return data
+
+
 
 def image_parser(args):
-    if os.path.isdir(args.image_folder):
-        image_files = [os.path.join(args.image_folder, f) for f in os.listdir(args.image_folder) if os.path.isfile(os.path.join(args.image_folder, f))]
-        return image_files
-    else:
-        return []
+    out = args.image_file.split(args.sep)
+    return out
+
 
 def load_image(image_file):
     if image_file.startswith("http") or image_file.startswith("https"):
@@ -45,6 +38,7 @@ def load_image(image_file):
     else:
         image = Image.open(image_file).convert("RGB")
     return image
+
 
 def load_images(images):
     width = max(img.width for img in images)
@@ -101,9 +95,8 @@ def process_image_tensor(image, image_processor, model):
 
     return image_tensor
 
-
-def run_inference(model, input_ids, image_tensor, args, tokenizer, caption):
-    stop_str = ', please answer me yes or no'
+def run_inference(model, input_ids, image_tensor, args, conv, tokenizer):
+    stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
     keywords = [stop_str]
     stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
     streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
@@ -120,10 +113,10 @@ def run_inference(model, input_ids, image_tensor, args, tokenizer, caption):
             stopping_criteria=[stopping_criteria])
 
     outputs = tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
-    
-    # Check if 'yes' or 'no' in the generated output
-    result = 1 if 'yes' in outputs.lower() else 0
-    return result
+    conv.messages[-1][-1] = outputs
+
+    if args.debug:
+        print("\n", {"prompt": prompt, "outputs": outputs}, "\n")
 
 def main(args):
     # Model
@@ -132,47 +125,127 @@ def main(args):
     model_name = get_model_name_from_path(args.model_path)
     tokenizer, model, image_processor, context_len = load_pretrained_model(args.model_path, args.model_base, model_name, args.load_8bit, args.load_4bit, device=args.device)
 
-    json_data = parse_jsonl(args.jsonl_file)
-    
-    correct_predictions = 0
-    total_predictions = 0
-    
-    for item in json_data:
-        image_file = item.get("image")
-        caption = item.get("caption")
-        label = item.get("label")
-        
-        if image_file is None or caption is None or label is None:
-            print(f"Invalid data entry: {item}. Skipping...")
-            continue
-        
-        image_path = os.path.join(args.image_folder, image_file)
-        if not os.path.isfile(image_path):
-            print(f"Image file not found: {image_path}. Skipping...")
-            continue
-        
-        image = load_image(image_path)
-        image_tensor = process_image_tensor(image, image_processor, model)
+    conv_mode = setup_conv_mode(model_name)
+    conv, roles = initialize_conv(conv_mode, model_name, args)
 
+    image = process_input_images(args)
+    image_tensor = process_image_tensor(image, image_processor, model)
+
+    while True:
         try:
-            inp = caption + ", please answer me yes or no"
+            inp = input(f"{roles[0]}: ")
         except EOFError:
             inp = ""
-            
-        prompt = inp
+        if not inp:
+            print("exit ")
+            break
+
+        print(f"{roles[1]}: ", end="")
+
+        if image is not None:
+            inp = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + inp if model.config.mm_use_im_start_end else DEFAULT_IMAGE_TOKEN + '\n' + inp
+            conv.append_message(conv.roles[0], inp)
+            image = None
+        else:
+            conv.append_message(conv.roles[0], inp)
+        conv.append_message(conv.roles[1], None)
+        prompt = conv.get_prompt()
+
         input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).to(model.device)
 
-        result = run_inference(model, input_ids, image_tensor, args, tokenizer, caption)
-        
-        total_predictions += 1
-        if result == label:
-            correct_predictions += 1
-        
-    accuracy = correct_predictions / total_predictions if total_predictions > 0 else 0
-    print(f"Accuracy: {accuracy * 100:.2f}% (Correct: {correct_predictions}, Total: {total_predictions})")
+        run_inference(model, input_ids, image_tensor, args, conv, tokenizer)
 
-    return accuracy
 
+def eval_model(args):
+    # Model
+    disable_torch_init()
+
+    model_name = get_model_name_from_path(args.model_path)
+    tokenizer, model, image_processor, context_len = load_pretrained_model(
+        args.model_path, args.model_base, model_name
+    )
+
+    qs = args.query
+    image_token_se = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN
+    if IMAGE_PLACEHOLDER in qs:
+        if model.config.mm_use_im_start_end:
+            qs = re.sub(IMAGE_PLACEHOLDER, image_token_se, qs)
+        else:
+            qs = re.sub(IMAGE_PLACEHOLDER, DEFAULT_IMAGE_TOKEN, qs)
+    else:
+        if model.config.mm_use_im_start_end:
+            qs = image_token_se + "\n" + qs
+        else:
+            qs = DEFAULT_IMAGE_TOKEN + "\n" + qs
+
+    if "llama-2" in model_name.lower():
+        conv_mode = "llava_llama_2"
+    elif "v1" in model_name.lower():
+        conv_mode = "llava_v1"
+    elif "mpt" in model_name.lower():
+        conv_mode = "mpt"
+    else:
+        conv_mode = "llava_v0"
+
+    if args.conv_mode is not None and conv_mode != args.conv_mode:
+        print(
+            "[WARNING] the auto inferred conversation mode is {}, while `--conv-mode` is {}, using {}".format(
+                conv_mode, args.conv_mode, args.conv_mode
+            )
+        )
+    else:
+        args.conv_mode = conv_mode
+
+    conv = conv_templates[args.conv_mode].copy()
+    conv.append_message(conv.roles[0], qs)
+    conv.append_message(conv.roles[1], None)
+    prompt = conv.get_prompt()
+
+    image_files = image_parser(args)
+    images = load_images(image_files)
+    images_tensor = process_images(
+        images,
+        image_processor,
+        model.config
+    ).to(model.device, dtype=torch.float16)
+
+    input_ids = (
+        tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt")
+        .unsqueeze(0)
+        .cuda()
+    )
+
+    stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+    keywords = [stop_str]
+    stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+
+    with torch.inference_mode():
+        output_ids = model.generate(
+            input_ids,
+            images=images_tensor,
+            do_sample=True if args.temperature > 0 else False,
+            temperature=args.temperature,
+            top_p=args.top_p,
+            num_beams=args.num_beams,
+            max_new_tokens=args.max_new_tokens,
+            use_cache=True,
+            stopping_criteria=[stopping_criteria],
+        )
+
+    input_token_len = input_ids.shape[1]
+    n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+    if n_diff_input_output > 0:
+        print(
+            f"[Warning] {n_diff_input_output} output_ids are not the same as the input_ids"
+        )
+    outputs = tokenizer.batch_decode(
+        output_ids[:, input_token_len:], skip_special_tokens=True
+    )[0]
+    outputs = outputs.strip()
+    if outputs.endswith(stop_str):
+        outputs = outputs[: -len(stop_str)]
+    outputs = outputs.strip()
+    print(outputs)
 
 
 
@@ -181,9 +254,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model-path", type=str, default="liuhaotian/llava-v1.5-13b")
     parser.add_argument("--model-base", type=str, default=None)
-    parser.add_argument("--image-folder", type=str, required=True)
-    parser.add_argument("--jsonl-file", type=str, required=True)
+    parser.add_argument("--images", type=str, nargs='+', required=True)
     parser.add_argument("--save-image", action="store_true")
+    parser.add_argument("--query", type=str, required=True)
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--conv-mode", type=str, default=None)
     parser.add_argument("--sep", type=str, default=",")
@@ -197,7 +270,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(args)
-
 
 
 
